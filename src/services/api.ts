73de307412e4bb937304651d78a9166c7e3d029e
MarcosEdington <115179086+MarcosEdington@users.noreply.github.com>
2026-02-12 00:38:30 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //baseURL: 'https://localhost:7064/api',  // Mude para a porta do seu backend se for diferente
+  //baseURL: 'https://localhost:7064/api',  //  backend se for diferente
   baseURL: 'https://controle-gastos-familiar-api.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
